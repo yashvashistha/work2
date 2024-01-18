c542b2af06a4ae14e341b96cf8cc347b32441155
YASH VASHISTHA <yashvashistha05@gmail.com>
2024-01-18 16:32:32 +0530

    Second commit

--- src/Screens/DragandDrop.js
+++ src/Screens/DragandDrop.js
@@ -1,40 +1,57 @@
 import React, { useRef, useState } from "react";
+import "./DragandDrop.css";
 
 const DragAndDrop = () => {
-  const [droppedFile1, setDroppedFile1] = useState(null);
-  const inputref = useRef();
+  const [divdisplay, setDivDisplay] = useState("none");
+  const divref = useRef();
 
-  const handleDragEnter = (e) => {
-    e.preventDefault();
-  };
+  // const handleDragEnter = (e) => {
+  //   e.preventDefault();
+  // };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
+  // const handleDragOver = (e) => {
+  //   e.preventDefault();
+  // };
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const filepdf = e.dataTransfer.files[0];
-    // console.log("File1 ->", e.dataTransfer.files[0]);
-    inputref.current.innerText = filepdf.name + " Added";
-  };
+  // const handleDrop = (e) => {
+  //   e.preventDefault();
+  //   const filepdf = e.dataTransfer.files[0];
+  //   // console.log("File1 ->", e.dataTransfer.files[0]);
+  //   inputref.current.innerText = filepdf.name + " Added";
+  // };
 
-  const submitfunc = () => {
-    console.log("clicked");
-    inputref.current.innerText = "Drag and Drop file";
+  // const submitfunc = () => {
+  //   console.log("clicked");
+  //   inputref.current.innerText = "Drag and Drop file";
+  // };
+
+  const openpopup = () => {
+    if (divdisplay == "none") {
+      divref.current.style.display = "flex";
+      setDivDisplay("flex");
+    } else if (divdisplay == "flex") {
+      divref.current.style.display = "none";
+      setDivDisplay("none");
+    }
+    return;
   };
 
   return (
-    <div>
-      <div
-        style={{ width: "250px", height: "250px", border: "2px dashed yellow" }}
-        onDragEnter={handleDragEnter}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-      >
-        <p ref={inputref}>Drag and Drop file</p>
+    <div className="maindiv">
+      <button className="Openpopup" onClick={openpopup}>
+        Click me please!!
+      </button>
+      <div ref={divref} className="Popup">
+        <button
+          className="closepopup"
+          style={{ position: "absolute", top: "0px", right: "0px" }}
+          onClick={openpopup}
+        >
+          X
+        </button>
+        <p>Drag and Drop</p>
+        <input type="file" />
       </div>
-      <button onClick={submitfunc}>Submit</button>
     </div>
   );
 };
